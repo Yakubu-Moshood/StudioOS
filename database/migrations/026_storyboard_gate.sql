8b4e99bd6fedeafffc93920ee3df67af719827da
Yakubu-Moshood <yakubu.mosh@gmail.com>
2026-07-03 16:00:30 +0100

    feat: add storyboard artifact type

---
+++ database/migrations/026_storyboard_gate.sql
@@ -0,0 +1,13 @@
+alter table public.artifacts drop constraint if exists artifacts_artifact_type_check;
+alter table public.artifacts add constraint artifacts_artifact_type_check check (
+  artifact_type = any (array[
+    'brief',
+    'research',
+    'script',
+    'production_package',
+    'big_creative_idea',
+    'concept_development',
+    'visual_development',
+    'storyboard'
+  ])
+);
